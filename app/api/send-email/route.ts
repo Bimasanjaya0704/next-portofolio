1fa0941cfc1ee662086349d54a22639bda9a3902
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-09-18 19:36:28 +0700

    feat : add api email testing using mailtrap in my project

---
+++ app/api/send-email/route.ts
@@ -0,0 +1,42 @@
+// app/api/send-email/route.ts
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export async function POST(request: Request) {
+  const { firstname, lastname, email, phone, message } = await request.json();
+
+  try {
+    await transporter.sendMail({
+      from: `"${firstname} ${lastname}" <${email}>`,
+      to: "bimasanjayawork@gmail.com",
+      subject: `New message from ${firstname} ${lastname}`,
+      text: message,
+      html: `<p><strong>First Name:</strong> ${firstname}</p>
+             <p><strong>Last Name:</strong> ${lastname}</p>
+             <p><strong>Email:</strong> ${email}</p>
+             <p><strong>Phone:</strong> ${phone}</p>
+             <p><strong>Message:</strong> ${message}</p>`,
+    });
+
+    return NextResponse.json(
+      { message: "Email sent successfully!" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Error sending email" },
+      { status: 500 }
+    );
+  }
+}
---
+++ components/ui/alert.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const alertVariants = cva(
+  "w-full rounded-lg border border-accent/30 p-4 mb-4",
+  {
+    variants: {
+      variant: {
+        success: "bg-accent text-primary",
+        error: "bg-red-500 text-white/70",
+      },
+    },
+    defaultVariants: {
+      variant: "success",
+    },
+  }
+);
+
+const Alert = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
+>(({ className, variant, ...props }, ref) => (
+  <div
+    ref={ref}
+    role="alert"
+    className={cn(alertVariants({ variant }), className)}
+    {...props}
+  />
+));
+Alert.displayName = "Alert";
+
+const AlertTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h5
+    ref={ref}
+    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+    {...props}
+  />
+));
+AlertTitle.displayName = "AlertTitle";
+
+const AlertDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    {...props}
+  />
+));
+AlertDescription.displayName = "AlertDescription";
+
+export { Alert, AlertTitle, AlertDescription };
