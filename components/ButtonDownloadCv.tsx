9fd21ea6c61e03efa4dd1f2339b9d8b7518e737b
Bima Sanjaya <122960409+Bimasanjaya0704@users.noreply.github.com>
2025-01-21 19:48:31 +0700

    update link file CV

--- components/ButtonDownloadCv.tsx
+++ components/ButtonDownloadCv.tsx
@@ -7,7 +7,7 @@ import { FiDownload } from "react-icons/fi";
 const ButtonDownloadCv = () => {
   const handleDownload = () => {
     const fileURL =
-      "https://drive.google.com/file/d/1F2xZA7BknEB3YsxnGrjQL5R5N8vf6TWB/view?usp=sharing";
+      "https://drive.google.com/file/d/1Vq2hAVRGby--TAsxADOQQw8__0SJKqZX/view?usp=sharing";
     window.open(fileURL, "_blank");
   };
 
