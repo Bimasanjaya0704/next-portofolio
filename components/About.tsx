62ac9fc124279b76732c519657aa11ec91e15e88
Bima Sanjaya <122960409+Bimasanjaya0704@users.noreply.github.com>
2025-02-20 11:21:16 +0700

    fix: update responsive in large display

--- components/About.tsx
+++ components/About.tsx
@@ -53,9 +53,9 @@ const About = () => {
     <section>
       <div className="bg-white dark:bg-[#232329] dark:border-accent dark:text-accent dark:border-opacity-5 border-4 py-8 md:py-24 text-primary px-4 lg:p-8 my-12 lg:mx-5 rounded-lg flex flex-col lg:flex-row items-center">
         {/* Text on the left */}
-        <div className="w-full lg:w-1/2 md:pr-8">
+        <div className="w-full flex flex-wrap md:flex-nowrap justify-center items-center gap-8">
           <div className="w-full">
-            <div className="text-2xl md:text-3xl lg:text-5xl font-bold text-center lg:text-left mb-4">
+            <div className="md:text-3xl lg:text-5xl 2xl:text-[100px] 2xl:mb-12 font-bold text-center lg:text-left mb-4">
               Career Journey
             </div>
 
@@ -78,39 +78,40 @@ const About = () => {
               ))}
             </div>
           </div>
-        </div>
-        {/* Text on the right */}
-        <div className="w-full lg:w-1/2 md:pl-8">
-          <div className="text-2xl mb-4 md:mb-0 md:text-3xl lg:text-5xl 2xl:text-[100px] 2xl:mb-12 font-bold text-center lg:text-right space-y-2">
-            Techstack
-          </div>
-          <p className="my-4 hidden md:block text-justify text-sm lg:text-base 2xl:text-xl dark:text-white dark:text-opacity-80">
-            As a Software Engineer, I possess a diverse skill set that drives
-            project success. I have been focusing on frontend development since
-            2020 and have been expanding my knowledge into backend technologies
-            since 2024. Here are some of the technologies and tools I use
-          </p>
 
-          <div className="relative w-full overflow-hidden">
-            <div className="flex w-max space-x-4 animate-loop-scroll">
-              {techList.concat(techList).map((service, index) => (
-                <div
-                  key={index}
-                  className="flex space-x-1 md:space-x-2 px-[10px] py-[5px] md:px-4 md:py-2 bg-accent text-dark dark:text-primary rounded-full shadow-md"
-                >
-                  <div className="flex items-center gap-2">
-                    <div className="text-[12px] md:text-base">
-                      {service.icon}
-                    </div>
-                    <div className="text-[12px] md:text-base">
-                      {service.name}
+          {/* Text on the right */}
+          <div className="w-full">
+            <div className="text-2xl mb-4 md:mb-0 md:text-3xl lg:text-5xl 2xl:text-[100px] 2xl:mb-12 font-bold text-center lg:text-right space-y-2">
+              Techstack
+            </div>
+            <p className="my-4 hidden md:block text-justify text-sm lg:text-base 2xl:text-xl dark:text-white dark:text-opacity-80">
+              As a Software Engineer, I possess a diverse skill set that drives
+              project success. I have been focusing on frontend development since
+              2020 and have been expanding my knowledge into backend technologies
+              since 2024. Here are some of the technologies and tools I use
+            </p>
+
+            <div className="relative w-full overflow-hidden">
+              <div className="flex w-max space-x-4 animate-loop-scroll">
+                {techList.concat(techList).map((service, index) => (
+                  <div
+                    key={index}
+                    className="flex space-x-1 md:space-x-2 px-[10px] py-[5px] md:px-4 md:py-2 bg-accent text-dark dark:text-primary rounded-full shadow-md"
+                  >
+                    <div className="flex items-center gap-2">
+                      <div className="text-[12px] md:text-base">
+                        {service.icon}
+                      </div>
+                      <div className="text-[12px] md:text-base">
+                        {service.name}
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
-        </div>
+        </div>        
       </div>
     </section>
   );
