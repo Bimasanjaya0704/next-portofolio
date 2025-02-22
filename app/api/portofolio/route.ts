c9369b98ac89deafa7e31cfc14b70098a988fdf0
Bimas <sanjabim77@gmail.com>
2025-02-22 21:43:21 +0700

    feat: setup api in backend

---
+++ app/api/portofolio/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { verifyToken } from "@/utils/authMiddleware";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const portfolios = await prisma.portfolio.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+
+    return NextResponse.json(
+      { success: true, data: portfolios },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const { title, description, imageUrl, projectUrl, liveUrl, token } =
+      await req.json();
+
+    const isAdmin = verifyToken(token);
+    if (!isAdmin) {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized" },
+        { status: 401 }
+      );
+    }
+
+    const userId = 1;
+    const newPortfolio = await prisma.portfolio.create({
+      data: { title, description, imageUrl, projectUrl, liveUrl },
+    });
+
+    return NextResponse.json(
+      { success: true, data: newPortfolio },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
