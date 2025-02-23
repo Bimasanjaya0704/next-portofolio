b49fe82ff642b3fbc87420133a7481d104d239ab
Bimas <sanjabim77@gmail.com>
2025-02-23 22:10:27 +0700

    update url and create interface model

---
+++ lib/types/interface.ts
@@ -0,0 +1,40 @@
+// src/lib/types/interface.ts
+
+export interface User {
+  id: number;
+  username: string;
+  password: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Blog {
+  id: number;
+  title: string;
+  content: string;
+  image: string;
+  createdAt: string;
+  updatedAt: string;
+  status?: string;
+}
+
+export interface Portfolio {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  projectUrl: string;
+  liveUrl: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Contact {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  message: string;
+  createdAt: string;
+}
