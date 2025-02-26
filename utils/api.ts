fc02099d4638964132e6aa2c038260ab27077522
Bimas <sanjabim77@gmail.com>
2025-02-26 21:35:34 +0700

    fix: update link

--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
+const API_BASE_URL = "https://bimasanjaya.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
