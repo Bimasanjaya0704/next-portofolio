fee0b9b43850b8f2cb4c21587fb195f3e3bca9b1
Bimas <sanjabim77@gmail.com>
2025-02-27 19:32:16 +0700

    feat: add design not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,20 +1,21 @@
 import Link from "next/link";
-import imageUnauthorized from "@/public/Unauthorized.svg";
+import imageNotFound from "@/public/not-found.svg";
 import Image from "next/image";
 import Footer from "@/components/Footer";
 import { Button } from "@/components/ui/button";
 
-export default function UnauthorizedPage() {
+export default function NotFoundPage() {
   return (
     <>
       <div className="container lg:p-10 bg-white flex flex-col md:flex-row items-center justify-around text-center md:text-left ">
         <div className="order-2 md:order-none">
           <h1 className="text-lg lg:text-5xl 2xl:text-[70px] font-black text-yellow-500 uppercase mb-4 md:mb-10">
-            You seem to be lost!
+            Oops! Page Not Found
           </h1>
           <p className="text-gray-600 text-sm md:text-base 2xl:text-xl">
-            The page youre looking for isnt available. Try searching again or
-            use the Go Back button below.
+            The page you are looking for might have been moved or does not
+            exist. Please check the URL or use the button below to return to the
+            homepage.
           </p>
           <Link href="/">
             <Button className="text-white mt-4 mb-8 2xl:text-2xl 2xl:px-8 2xl:py-8">
@@ -23,7 +24,11 @@ export default function UnauthorizedPage() {
           </Link>
         </div>
         <div className="text-right order-1 md:order-none">
-          <Image src={imageUnauthorized} alt="unaothorized" />
+          <Image
+            src={imageNotFound}
+            alt="not found"
+            className="md:w-[900px] 2xl:w-[1000px]"
+          />
         </div>
       </div>
       <Footer />
